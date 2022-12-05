86ed63153db4eafe527b5eab9ae883df75cc34bd
Armin Ghavidel <a.ghavidel@authin.ir>
2022-12-06 00:20:40 +0330

    first commit

---
+++ src/main/java/com/example/classlevelconstraint/ClassLevelConstraintApplication.java
@@ -0,0 +1,13 @@
+package com.example.classlevelconstraint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassLevelConstraintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassLevelConstraintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classlevelconstraint/ClassLevelConstraintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classlevelconstraint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassLevelConstraintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
